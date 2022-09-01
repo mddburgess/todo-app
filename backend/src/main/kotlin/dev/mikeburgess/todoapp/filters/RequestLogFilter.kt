a54a48cdf705bdacf5abd90c42d156caaebbca99
Mike Burgess <mddburgess@users.noreply.github.com>
2022-08-31 20:07:16 -0700

    Enhance request log filter.

--- backend/src/main/kotlin/dev/mikeburgess/todoapp/filters/RequestLogFilter.kt
+++ backend/src/main/kotlin/dev/mikeburgess/todoapp/filters/RequestLogFilter.kt
@@ -1,6 +1,7 @@
 package dev.mikeburgess.todoapp.filters
 
 import mu.KotlinLogging
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.core.annotation.Order
 import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Component
@@ -13,14 +14,17 @@ private val log = KotlinLogging.logger { }
 
 @Component
 @Order(Filters.REQUEST_LOG)
-class RequestLogFilter : OncePerRequestFilter() {
+class RequestLogFilter(
+    @Value("\${api.base-path:/api/v1}")
+    private val apiBasePath: String
+) : OncePerRequestFilter() {
 
     override fun doFilterInternal(
         request: HttpServletRequest,
         response: HttpServletResponse,
         filterChain: FilterChain
     ) {
-        val shouldLog = request.requestURI.startsWith("/api")
+        val shouldLog = request.requestURI.startsWith(apiBasePath)
 
         try {
             if (shouldLog) {
