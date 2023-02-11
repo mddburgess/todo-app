d5c409af29bc255684cec519d6ae1aa9f6c3cdf6
Mike Burgess <mddburgess@users.noreply.github.com>
2023-02-10 17:17:43 -0800

    Refactor frontend build.

--- frontend/build.gradle.kts
+++ frontend/build.gradle.kts
@@ -1,15 +1,54 @@
-plugins {
-    id("org.siouan.frontend-jdk11") version "6.0.0"
-}
+import com.github.gradle.node.npm.task.NpmTask
+import com.github.gradle.node.npm.task.NpxTask
 
-frontend {
-    nodeVersion.set("18.4.0")
-    cleanScript.set("run clean")
-    assembleScript.set("run assemble")
+plugins {
+    id("com.github.node-gradle.node") version "3.5.1"
 }
 
 tasks {
-    assembleFrontend {
+    register<Delete>("clean") {
+        delete("dist", "src/api")
+    }
+
+    register<NpxTask>("npmCheckUpdates") {
         dependsOn(project(":api").tasks.named("validate"))
+        command.set("npm-check-updates")
+        args.add("-u")
+    }
+
+    npmInstall {
+        dependsOn("npmCheckUpdates")
+    }
+
+    register<NpxTask>("codegenOpenApi") {
+        dependsOn("npmInstall")
+        inputs.files(
+            project(":api").fileTree("src"),
+            "openapi-config.json"
+        )
+        outputs.dir("src/api")
+        command.set("@rtk-query/codegen-openapi")
+        args.add("openapi-config.json")
+    }
+
+    register<NpmTask>("validateTypescript") {
+        dependsOn("codegenOpenApi")
+        npmCommand.addAll("run", "validate")
+    }
+
+    register<NpmTask>("assemble") {
+        dependsOn("validateTypescript")
+        inputs.files(
+            fileTree("node_modules"),
+            fileTree("src"),
+            "package.json",
+            "tsconfig.json"
+        )
+        outputs.dir("dist")
+        npmCommand.addAll("run", "build")
+    }
+
+    register<DefaultTask>("build") {
+        dependsOn("assemble")
     }
 }
