fe0307e7b23b42ff7165ab27f35748cc92a41ecd
Mike Burgess <mddburgess@users.noreply.github.com>
2023-02-13 21:47:09 -0800

    Add delete button to todo item row.

--- frontend/src/components/TodoItemRow.tsx
+++ frontend/src/components/TodoItemRow.tsx
@@ -1,12 +1,25 @@
 import {TodoItem} from "../api/todoApi";
 import ListGroupItem from "react-bootstrap/ListGroupItem";
+import {Trash3} from "react-bootstrap-icons";
+import {Button, Col, Row} from "react-bootstrap";
 
 interface Props {
     item: TodoItem
 }
 
 const TodoItemRow = ({item}: Props) => (
-    <ListGroupItem>{item.summary}</ListGroupItem>
+    <ListGroupItem>
+        <Row>
+            <Col className="d-flex align-items-center">
+                {item.summary}
+            </Col>
+            <Col xs="auto" className="px-1">
+                <Button variant="outline-danger" className="d-flex align-items-center">
+                    <Trash3/>
+                </Button>
+            </Col>
+        </Row>
+    </ListGroupItem>
 )
 
 export default TodoItemRow;
